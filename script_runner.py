363c22ea4dba0f64398051f231125a1699e78c5e
Alena Stavrova <33433876+Alena-Stavrova@users.noreply.github.com>
2026-03-27 12:19:37 +0300

    Added ES LVH, upated runner
    Wrote ES LVH. Payment logic is still unclear (120+ eu + cash), will add later

--- script_runner.py
+++ script_runner.py
@@ -1,7 +1,7 @@
 #import BG_random_V2 as bg
 import CZ_random_V2 as cz
 import DE_random_V2 as de
-#import ES_random_V2 as es
+import ES_random_V2 as es
 import EU_random_V2 as eu
 #import HU_random_V2 as hu
 import IT_random_V2 as it
@@ -15,7 +15,7 @@ script_modules = {
     #'BG': bg,
     'CZ': cz,
     'DE': de,
-    #'ES': es,IT
+    'ES': es,
     'EU': eu,
     #'HU': hu,
     'IT': it,
